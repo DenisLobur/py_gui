fe607c4401efb7a65488bdc11cee68d655258f6e
denislobur <denisdenisdenis@bigmir.net>
2017-03-02 18:06:47 +0200

    add first row of buttons (GUI only)

--- Calculator.py
+++ Calculator.py
@@ -13,7 +13,22 @@ class Application(Frame):
     def createWidgets(self):
         self.display = Entry(self, font=("Helvetica", 16), relief=RAISED, justify=RIGHT)
         self.display.insert(0, "0")
-        self.display.grid(row=0, column=0)
+        self.display.grid(row=0, column=0, columnspan=5)
+
+        self.sevenButton = Button(self, font=("Helvetica", 11), text="7", borderwidth=1)
+        self.sevenButton.grid(row=1, column=0, sticky="NWNESWSE")
+
+        self.eightButton = Button(self, font=("Helvetica", 11), text="8", borderwidth=1)
+        self.eightButton.grid(row=1, column=1, sticky="NWNESWSE")
+
+        self.nineButton = Button(self, font=("Helvetica", 11), text="9", borderwidth=1)
+        self.nineButton.grid(row=1, column=2, sticky="NWNESWSE")
+
+        self.timesButton = Button(self, font=("Helvetica", 11), text="*", borderwidth=1)
+        self.timesButton.grid(row=1, column=3, sticky="NWNESWSE")
+
+        self.clearButton = Button(self, font=("Helvetica", 11), text="C", borderwidth=1)
+        self.clearButton.grid(row=1, column=4, sticky="NWNESWSE")
 
 
 app = Application(root).grid()
